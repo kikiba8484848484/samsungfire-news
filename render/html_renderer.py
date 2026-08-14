3d1d5fc45d4e6e90371c9cda7b18f4e230e2c2e1
kikiba8484848484 <kikiba84@gmail.com>
2026-08-14 10:42:10 +0900

    Update html_renderer.py

--- render/html_renderer.py
+++ render/html_renderer.py
@@ -1,20 +1,11 @@
 from datetime import datetime
 from pathlib import Path
-from collections import OrderedDict
 from jinja2 import Environment, FileSystemLoader
 
 from settings import TEMPLATE_DIR, OUTPUT_HTML_DIR, now_kst
 
 
-def _group_by_country(overview_articles: list) -> "OrderedDict":
-    """개요 기사를 국가별로 묶는다. 첫 등장 순서(중요도순)를 그대로 그룹 순서로 사용한다."""
-    grouped = OrderedDict()
-    for a in overview_articles:
-        grouped.setdefault(a.country, []).append(a)
-    return grouped
-
-
-def render_html(articles_by_country: dict, overview_articles: list, local_affairs_top1: dict) -> Path:
+def render_html(articles_by_country: dict, local_affairs_top1: dict) -> Path:
     env = Environment(loader=FileSystemLoader(str(TEMPLATE_DIR)))
     template = env.get_template("cardnews.html.j2")
 
@@ -22,7 +13,6 @@ def render_html(articles_by_country: dict, overview_articles: list, local_affair
     html = template.render(
         report_date=report_date,
         countries=articles_by_country,
-        overview_by_country=_group_by_country(overview_articles),
         local_affairs_top1=local_affairs_top1,
     )
 
